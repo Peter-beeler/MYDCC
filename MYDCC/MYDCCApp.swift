02243b9d365ab5f837db1dce57d8e80c3a12dce5
maoyi <429744889@qq.com>
2025-05-06 21:21:59 -0400

    Initial Commit

---
+++ MYDCC/MYDCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYDCCApp.swift
+//  MYDCC
+//
+//  Created by mao.496 on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MYDCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MYDCCTests/MYDCCTests.swift
@@ -0,0 +1,17 @@
+//
+//  MYDCCTests.swift
+//  MYDCCTests
+//
+//  Created by mao.496 on 5/6/25.
+//
+
+import Testing
+@testable import MYDCC
+
+struct MYDCCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
